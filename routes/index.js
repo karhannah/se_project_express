067dbc2fe183556ef28e6e02757dcfb181f39686
Karinne Hannah <158782083+karhannah@users.noreply.github.com>
2025-03-05 19:15:50 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const router = require( 'express' ).Router();
 const path = require( 'path' );
 
-const userRouter = require( './users.js' );
-const itemRouter = require( './clothingItems.js' );
+const userRouter = require( './users' );
+const itemRouter = require( './clothingItems' );
 
 router.use( '/users', userRouter );
 router.use( '/items', itemRouter );
 
 router.use(( req, res ) => {
-	res.status( 500 ).send({ message: 'Path not found' });
+	res.status( 404 ).send({ message: 'Path not found' });
 })
 
 module.exports = router;
