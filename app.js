cda1bfe20ced6951c7d75f631ce932dbc6ff7416
Karinne Hannah <158782083+karhannah@users.noreply.github.com>
2025-03-05 19:16:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require( 'mongoose' );
 const app = express();
 const { PORT = 3001 } = process.env;
 
-const mainRouter = require( './routes/index.js' );
+const mainRouter = require( './routes/index' );
 
 mongoose
 	.connect( 'mongodb://127.0.0.1:27017/wtwr_db' )
