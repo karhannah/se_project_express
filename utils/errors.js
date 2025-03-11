12d4ebdafe7e0fedc252739cb00dc40753ce0b85
Karinne Hannah <158782083+karhannah@users.noreply.github.com>
2025-03-11 13:19:03 -0400

    Update errors.js

--- utils/errors.js
+++ utils/errors.js
@@ -1,5 +1,5 @@
 module.exports = {
-	ERR_400_BADPARAMS: { message: 'invalid data passed to the methods for creating an item/user or updating an item, or invalid ID passed to the params.' },
-	ERR_404_NOTFOUND: { message: 'there is no user or clothing item with the requested id, or the request was sent to a non-existent address.' },
-	ERR_500_INTERNAL: { message: 'An error has occurred on the server.' }
+	BAD_REQUEST: { code: 400, message: 'invalid data passed to the methods for creating an item/user or updating an item, or invalid ID passed to the params.' },
+	NOT_FOUND: { code: 404, message: 'there is no user or clothing item with the requested id, or the request was sent to a non-existent address.' },
+	DEFAULT: { code: 500, message: 'An error has occurred on the server.' }
 }
